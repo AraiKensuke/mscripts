440f698d8248dfd3a03608bc70ef01f60c335b68
KensukeArai <kensuke.y.arai@gmail.com>
2014-08-06 18:54:11 -0400

    Deleted up some old files

--- cyth.py
+++
@@ -1,8 +0,0 @@
-def tf():
-    j = 0
-    for i in xrange(1000000):
-        j = _N.sin(i)
-
-    print j
-
-
--- fact.py
+++
@@ -1,6 +0,0 @@
-nn = 1.
-
-for n in xrange(1, 60):
-    #  start with 1!
-    nn *= n
-    print "%(1).5e %(2).5e" % {"1" : _N.log(nn), "2" : (n*_N.log(n) - n)}
