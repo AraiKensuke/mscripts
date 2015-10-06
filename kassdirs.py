af6a966fb242cbcab5b77db4c6dd0f8b4cea819d
KensukeArai <kensuke.y.arai@gmail.com>
2015-10-06 12:32:15 -0400

    Marginal MVN

--- kassdirs.py
+++ kassdirs.py
@@ -1,21 +1,12 @@
 import os
 
-"""
-def resFN(fn, dir=None, create=False):
+def resFN(fn, dir=None, create=False, env_dirname=None):
     #  ${Kassdir}/Results/
-    __kassResultDir__ = os.environ["__kassResultDir__"]
-    rD = __kassResultDir__
+    #  dir is inside the result directory
+    #  look inside __kassResultDir__ env var.
+    env_dirname="__kassResultDir__" if (env_dirname==None) else env_dirname
 
-    if dir != None:
-        rD = "%(rd)s/%(ed)s" % {"rd" : __kassResultDir__, "ed" : dir}
-        if not os.access("%s" % rD, os.F_OK) and create:
-            os.mkdir(rD)
-    return "%(rd)s/%(fn)s" % {"rd" : rD, "fn" : fn}
-"""
-
-def resFN(fn, dir=None, create=False):
-    #  ${Kassdir}/Results/
-    __kassResultDir__ = os.environ["__kassResultDir__"]
+    __kassResultDir__ = os.environ[env_dirname]
     rD = __kassResultDir__
 
     if dir != None:
@@ -25,30 +16,6 @@ def resFN(fn, dir=None, create=False):
             if not os.access("%s" % rD, os.F_OK) and create:
                 os.mkdir(rD)
     return "%(rd)s/%(fn)s" % {"rd" : rD, "fn" : fn}
-
-def pracFN(fn, dir=None, create=False):
-    #  ${Kassdir}/Results/
-    __kassPracDir__ = os.environ["__kassPracDir__"]
-    rD = __kassPracDir__
-
-    if dir != None:
-        lvls = dir.split("/")
-        for lvl in lvls:
-            rD += "/%s" % lvl
-            if not os.access("%s" % rD, os.F_OK) and create:
-                os.mkdir(rD)
-    return "%(rd)s/%(fn)s" % {"rd" : rD, "fn" : fn}
-
-def prcmpFN(fn, dir=None, create=False):
-    #  ${Kassdir}/Results/
-    __kassPrecompDir__ = os.environ["__kassPrecompDir__"]
-    pD = __kassPrecompDir__
-
-    if dir != None:
-        pD = "%(rd)s/%(ed)s" % {"rd" : __kassPrecompDir__, "ed" : dir}
-        if not os.access("%s" % pD, os.F_OK) and create:
-            os.mkdir(pD)
-    return "%(rd)s/%(fn)s" % {"rd" : pD, "fn" : fn}
 
 def datFN(fn, dir=None, create=False):
     #  ${Kassdir}/Results/
--- mvn.py
+++ mvn.py
@@ -1,10 +1,38 @@
 #  given covariance matrix and list of indices, tell me what the 
+import numpy as _N
 
-def marginalPDF(cov, lndx):
+
+def marginalPDF(cov, lndx, cond_on_this=None):
     """
     cov    covariance matrix 
     lndx   list of indices to marginalize over
+
+    #  For given values of 
     """
+    dim = cov.shape[0]
+    
+    allind = _N.arange(dim)
+    ulndx= _N.setdiff1d(allind, lndx)
+
+    dimMO= lndx.shape[0]
+    dimM = ulndx.shape[0]
+    reord= _N.empty((dim, dim))
+    
+    A    = cov[_N.ix_(ulndx,  ulndx)]
+    B    = cov[_N.ix_(lndx,    lndx)]
+    C    = cov[_N.ix_(lndx,   ulndx)]
+
+    reord[0:dimM, 0:dimM] = A
+    reord[dimM:,  dimM:]  = B
+    B[0:dimM, dimM:]      = C.T
+    B[dimM:,  0:dimM]     = C
+
+    if cond_on_this is None:
+        return A, B, C
+    else:
+        Bi = _N.linalg.inv(B)
+        us = _N.dot(_N.dot(C.T, Bi), cond_on_this)
+        return A, B, C, us, A - _N.dot(C.T, _N.dot(Bi, C))
 
 def practice():    
     #  remove rows and columns from matrix
